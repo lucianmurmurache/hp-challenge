84ad8aea99d16060cc01f0121c500c61c7c0b413
lucianmurmurache <lucianmurmurache@gmail.com>
2024-03-17 19:52:03 +0100

    chore: complete button functionality

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,13 +1,9 @@
-function Button() {
-  /* 
-    TODO: There will be several buttons
-    Can pass label and onClick 
-    */
-  return (
-    <button onClick={() => console.log("some event")}>
-      <span>LABEL</span>
-    </button>
-  );
+function Button({ label, onClick, disabled }: ButtonProps) {
+    return (
+        <button onClick={onClick} disabled={disabled} className="button">
+            <span>{label}</span>
+        </button>
+    );
 }
 
 export default Button;
