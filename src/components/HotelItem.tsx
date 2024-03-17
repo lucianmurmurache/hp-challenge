0e4ad49d3f5edece774c7a1b65c0130cd6f154d0
lucianmurmurache <lucianmurmurache@gmail.com>
2024-03-17 19:53:43 +0100

    chore: add content, stars and reviews

--- src/components/HotelItem.tsx
+++ src/components/HotelItem.tsx
@@ -1,5 +1,15 @@
 import { useState } from 'react';
+import { formatDate } from '../utils/formatDate';
 import { documentToPlainTextString } from '@contentful/rich-text-plain-text-renderer';
+import type { Hotel } from '../App';
+import Loading from './common/Loading';
+import Button from './common/Button';
+import Stars from './Stars';
+import Reviews from './Reviews';
+
+type HotelItemProps = {
+    hotel: Hotel;
+};
 
 function HotelItem({ hotel }: HotelItemProps) {
     const [showReviews, setShowReviews] = useState(false);
@@ -8,34 +18,70 @@ function HotelItem({ hotel }: HotelItemProps) {
         setShowReviews((prevState) => !prevState);
     };
 
-    const { name, description } = hotel;
+    const {
+        sys,
+        name,
+        description,
+        city,
+        country,
+        rating,
+        price,
+        startDate,
+        endDate,
+        imagesCollection: {
+            items: [firstImage],
+        },
+    } = hotel;
 
-    if (!hotel) return <>Loading...</>;
+    if (!hotel) return <Loading />;
 
     return (
         <div className="hotel-item-wrapper">
             <div className="grid grid-cols-1 md:grid-cols-5">
+                <img
+                    src={firstImage.url}
+                    alt={firstImage.title}
+                    title={firstImage.title}
+                    className="h-full w-full md:col-span-2"
+                />
+
                 <div className=" relative p-5 md:col-span-3 md:p-8">
                     <h2>{name}</h2>
-
-                    <p className="hyphen-auto my-4 line-clamp-6">
-                        {documentToPlainTextString(description)} {/* WIP */}
+                    <p>
+                        {city} - {country}
                     </p>
+                    <p className="hyphen-auto my-4 line-clamp-6">
+                        {documentToPlainTextString(description.json)}
+                    </p>
+
+                    <div className="absolute right-5 top-5">
+                        <Stars rating={rating} />
+                    </div>
+                    <div className="flex flex-row-reverse justify-between">
+                        <div className="text-right">
+                            <p className="text-2xl font-semibold">
+                                {price.value} {price.symbol}
+                            </p>
+                            <small>
+                                {formatDate(startDate)} - {formatDate(endDate)}
+                            </small>
+                        </div>
+                        <div className="my-auto">
+                            <Button
+                                label={
+                                    showReviews
+                                        ? 'Hide reviews'
+                                        : 'Show reviews'
+                                }
+                                onClick={toggleReviews}
+                            />
+                        </div>
+                    </div>
                 </div>
-                <button onClick={toggleReviews}>
-                    {showReviews ? 'Hide reviews' : 'Show reviews'}
-                </button>
             </div>
+
+            {showReviews ? <Reviews hotelId={sys.id} /> : null}
         </div>
     );
 }
 export default HotelItem;
-
-type HotelInfo = {
-    name: string;
-    description: string;
-};
-
-type HotelItemProps = {
-    hotel: HotelInfo;
-};
